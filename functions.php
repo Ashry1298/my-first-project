9303c282dccb7f4433ca16521d70a9526f4a708c
Ashry11 <108164901+Ashry1298@users.noreply.github.com>
2023-01-02 02:40:21 +0200

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,14 +1,6 @@
 <?php
 
-// function checkRequestMethod($method)
-// {
 
-//     if ($_SERVER['REQUEST_METHOD'] == $method) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
 function checkRequestMethod($method)
 {
     if ($_SERVER['REQUEST_METHOD'] == $method) {
@@ -26,36 +18,14 @@ function checkrequestkey($key)
     }
 }
 
-// function checkPostInput($input)
-// {
 
-//     if (isset($_POST[$input])) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
 function sanitizeINputs($input)
 {
     return trim(htmlspecialchars(htmlentities($input)));
 }
 
-// function sanitizeINputs($input)
-// {
-
-//   return trim(htmlspecialchars(htmlentities($input)));
-// }
 
 function redirect($input)
 {
     header("location:path");
 }
-// function checkPasswordLogin($key1,$key2)
-// {  
-
-//     if ($_POST['email'] == $key1 || sha1($_POST['password'] == $key2) {
-
-//         return true;
-//     }else
-//     return false;
-// }
